65dbca3bd54941b5bbe9dc4757667ca085f80909
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-27 16:24:53 +0100

    added href

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import Sound from "../assets/sound.wav";
+import { Link } from "react-router-dom";
 
 function Home() {
   const playSound = () => {
@@ -12,11 +13,12 @@ function Home() {
       <header className="App-header">
         {/* <button onClick={playSound}>TITLE PAGE HERE</button> */}
       </header>
-
+      <a href="/ask">
       <div className='header-section AskGod'>
           <div className="Ask omfg" style={{ fontSize: '380px', paddingTop: "0px" }}>ASK</div>
           <div className="God omfg" style={{ fontSize: '380px', paddingTop: "0px" }}>GOD!</div>
         </div>
+        </a>
     </div>
   );
 }

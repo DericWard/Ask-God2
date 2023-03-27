c8f8cd1c8adf6d1f5038c10f5845c9df0acab8ed
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-03-27 12:43:53 +0200

    function generating random quotes with API

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Ask from "./pages/Ask.js";
 import History from "./pages/History";
+import axios from 'axios';
+
 
 function App() {
+
+// API call for random quotes
+  const getQuote = () => {
+    axios.get('https://api.quotable.io/random')
+    .then((response) => {
+    console.log(response.data.content);
+    console.log(response.data.author);
+})
+    .catch((error) => {
+    console.log(error);
+})
+  }
+
+
+  
   return (
     <Router>
       <ResponsiveAppBar />
@@ -17,6 +34,7 @@ function App() {
         <Route path="/ask" element={<Ask />} />
         <Route path="/history" element={<History />} />
       </Routes>
+      <button onClick={getQuote}>Get Quote</button>
     </Router>
   );
 }

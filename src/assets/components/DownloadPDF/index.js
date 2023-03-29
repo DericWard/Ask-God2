417db994312dcd9e715399f4670a792cb298e381
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-29 21:50:54 +0100

    added css for button at mobile res

--- src/assets/components/DownloadPDF/index.js
+++ src/assets/components/DownloadPDF/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  Button  from "@mui/material/Button";
+import Button from "@mui/material/Button";
 import { jsPDF } from "jspdf";
 
 function DownloadPDF({ data }) {
@@ -10,18 +10,20 @@ function DownloadPDF({ data }) {
       doc.text(`${index + 1}. ${question}`, 10, 10 + index * 30);
       doc.text(answer, 20, 15 + index * 30);
     });
-    
 
     doc.save("history.pdf");
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center"}}>
-      <Button variant="contained" onClick={generatePDF} sx={{backgroundColor: "#2d7cb2",}}>
-        Download Your Chats with God!
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Button
+        variant="contained"
+        onClick={generatePDF}
+        sx={{ backgroundColor: "#2d7cb2" }}
+      >
+        Download Chats
       </Button>
     </div>
-
   );
 }
 

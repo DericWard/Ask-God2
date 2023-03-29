1d5157c3cdf07e9a81fa18d4e44c1023897efcb1
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-29 22:26:59 +0100

    new pdf check

--- src/assets/components/DownloadPDF/index.js
+++ src/assets/components/DownloadPDF/index.js
@@ -1,30 +1,44 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { jsPDF } from "jspdf";
+import { useState } from "react";
 
 function DownloadPDF({ data }) {
+  const [y, setY] = useState(10);
+
   const generatePDF = () => {
     const doc = new jsPDF();
+    const pageHeight = doc.internal.pageSize.height;
 
-    let y = 10;
-    data.forEach(({ question, answer }, index) => {
-      if (index === 1) {
-        y += 10; // Add extra space after first question
-        doc.line(10, y, 200, y); // Draw horizontal line
-        y += 10; // Add extra space after line
-      }
+    let newY = y; // use a local variable to track the current y position
+
+    data.map(({ question, answer }, index) => {
+      // Check if content exceeds current page height
       const questionLines = doc.splitTextToSize(`${index + 1}. ${question}`, 180);
       const answerLines = doc.splitTextToSize(answer, 180);
       const lines = Math.max(questionLines.length, answerLines.length);
+      const contentHeight = lines * 10;
+      if (newY + contentHeight > pageHeight - 10) {
+        doc.addPage();
+        newY = 10; // reset the local variable when a new page is added
+      }
+
+      // Add space between each question-answer pair
+      if (index > 0) {
+        newY += 10; // increment the local variable, not the state variable
+      }
+
+      // Format and render question-answer pair
       for (let i = 0; i < lines; i++) {
         const questionLine = questionLines[i] || '';
         const answerLine = answerLines[i] || '';
-        doc.text(questionLine, 10, y);
-        doc.text(answerLine, 20, y + 5);
-        y += 10;
+        doc.text(questionLine, 10, newY);
+        doc.text(answerLine, 20, newY + 5);
+        newY += 10; // increment the local variable, not the state variable
       }
     });
 
+    setY(newY); // update the state variable after the loop is finished
     doc.save("history.pdf");
   };
 
